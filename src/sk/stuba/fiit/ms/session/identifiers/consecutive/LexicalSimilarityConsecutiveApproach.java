62a809727d25d32f3b56b4d43c44c7bfbdab4050
Martin Petluš <martinpetlus@gmail.com>
2015-04-24 00:35:34 +0200

    Fix LexicalSimilarityConsecutiveApproach

--- src/sk/stuba/fiit/ms/session/identifiers/consecutive/LexicalSimilarityConsecutiveApproach.java
+++ src/sk/stuba/fiit/ms/session/identifiers/consecutive/LexicalSimilarityConsecutiveApproach.java
@@ -1,10 +1,9 @@
 package sk.stuba.fiit.ms.session.identifiers.consecutive;
 
 import sk.stuba.fiit.ms.session.Search;
-import sk.stuba.fiit.ms.similarities.lexical.CosineLexicalSimilarity;
 import sk.stuba.fiit.ms.similarities.lexical.LexicalSimilarity;
 
-class LexicalSimilarityConsecutiveApproach {
+class LexicalSimilarityConsecutiveApproach implements  ConsecutiveApproach {
 
     private final LexicalSimilarity lexicalSimilarity;
 
@@ -12,6 +11,7 @@ class LexicalSimilarityConsecutiveApproach {
         this.lexicalSimilarity = lexicalSimilarity;
     }
 
+    @Override
     public final boolean isSameSession(final Search previous, final Search current) {
         return lexicalSimilarity.calculate(previous.getQuery(), current.getQuery()) > 0.5;
     }
