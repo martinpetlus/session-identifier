dc8b98baa5131dc7191d55478990da05b1d033e7
Martin Petluš <martinpetlus@gmail.com>
2015-04-23 23:10:49 +0200

    Implement CosineSimilarityConsecutiveApproach

--- src/sk/stuba/fiit/ms/session/identifiers/consecutive/CosineSimilarityConsecutiveApproach.java
+++ src/sk/stuba/fiit/ms/session/identifiers/consecutive/CosineSimilarityConsecutiveApproach.java
@@ -1,12 +1,20 @@
 package sk.stuba.fiit.ms.session.identifiers.consecutive;
 
 import sk.stuba.fiit.ms.session.Search;
+import sk.stuba.fiit.ms.similarities.lexical.CosineLexicalSimilarity;
+import sk.stuba.fiit.ms.similarities.lexical.LexicalSimilarity;
 
 public final class CosineSimilarityConsecutiveApproach implements ConsecutiveApproach {
 
+    private final LexicalSimilarity lexicalSimilarity;
+
+    public CosineSimilarityConsecutiveApproach() {
+        lexicalSimilarity = CosineLexicalSimilarity.newInstance();
+    }
+
     @Override
     public boolean isSameSession(final Search previous, final Search current) {
-        return false;
+        return lexicalSimilarity.calculate(previous.getQuery(), current.getQuery()) > 0.5;
     }
 
 }
