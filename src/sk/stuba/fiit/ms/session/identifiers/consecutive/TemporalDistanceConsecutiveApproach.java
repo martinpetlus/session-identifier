505cb3cf317c54d9f75484d4abab2eced952607a
Martin Petluš <martinpetlus@gmail.com>
2015-04-23 21:37:29 +0200

    Fix class declaration

--- src/sk/stuba/fiit/ms/session/identifiers/consecutive/TemporalDistanceConsecutiveApproach.java
+++ src/sk/stuba/fiit/ms/session/identifiers/consecutive/TemporalDistanceConsecutiveApproach.java
@@ -2,13 +2,13 @@ package sk.stuba.fiit.ms.session.identifiers.consecutive;
 
 import sk.stuba.fiit.ms.session.Search;
 
-public class TemporalDistanceConsecutiveApproach implements ConsecutiveApproach {
+public final class TemporalDistanceConsecutiveApproach implements ConsecutiveApproach {
 
     // 25.5 minutes
     public static final int MAX_GAP_IN_SECONDS = 1530;
 
     @Override
-    public boolean isSameSession(Search previous, Search current) {
+    public boolean isSameSession(final Search previous, final Search current) {
         return (current.getTimeStamp() - previous.getTimeStamp()) <= MAX_GAP_IN_SECONDS;
     }
 
