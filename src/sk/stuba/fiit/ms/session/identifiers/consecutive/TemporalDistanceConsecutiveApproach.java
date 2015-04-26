3a1dcc4aa344e4f43a9e22a3a6c45bafd050aad7
Martin Petluš <martinpetlus@gmail.com>
2015-04-26 11:47:58 +0200

    Fix maximum temporal difference constant to be in milliseconds

--- src/sk/stuba/fiit/ms/session/identifiers/consecutive/TemporalDistanceConsecutiveApproach.java
+++ src/sk/stuba/fiit/ms/session/identifiers/consecutive/TemporalDistanceConsecutiveApproach.java
@@ -5,11 +5,11 @@ import sk.stuba.fiit.ms.session.Search;
 public final class TemporalDistanceConsecutiveApproach implements ConsecutiveApproach {
 
     // 25.5 minutes
-    public static final int MAX_GAP_IN_SECONDS = 1530;
+    public static final long MAX_GAP_IN_MILLIS = 1530_000L;
 
     @Override
     public boolean isSameSession(final Search previous, final Search current) {
-        return (current.getTimeStamp() - previous.getTimeStamp()) <= MAX_GAP_IN_SECONDS;
+        return (current.getTimeStamp() - previous.getTimeStamp()) <= MAX_GAP_IN_MILLIS;
     }
 
 }
