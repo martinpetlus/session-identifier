94f3cf91246027389c740ab5c8cfd985fc217569
Martin Petluš <martinpetlus@gmail.com>
2015-04-28 23:16:06 +0200

    Remove splitQueriesInTestingSessions method

--- src/sk/stuba/fiit/ms/learning/test/SessionsSeparator.java
+++ src/sk/stuba/fiit/ms/learning/test/SessionsSeparator.java
@@ -1,11 +1,9 @@
 package sk.stuba.fiit.ms.learning.test;
 
-import sk.stuba.fiit.ms.session.Search;
 import sk.stuba.fiit.ms.session.Session;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public final class SessionsSeparator {
 
@@ -27,47 +25,6 @@ public final class SessionsSeparator {
 
         trainingSessions = new ArrayList<Session>(sessions.subList(0, numberOfTrainingSessions));
         testingSessions = new ArrayList<Session>(sessions.subList(numberOfTrainingSessions, sessions.size()));
-    }
-
-    public void splitQueriesInTestingSessions() {
-        List<Session> newTestingSessions = new ArrayList<Session>();
-
-        Random random = new Random();
-
-        for (Session session : testingSessions) {
-            Session trainSession = copySession(session);
-            Session testSession = copySession(session);
-
-            List<Search> searches = session.getAllSearches();
-
-            for (int i = 0; i < searches.size(); i++) {
-                int randomIndex = random.nextInt(searches.size());
-
-                trainSession.add(searches.get(randomIndex));
-                searches.remove(randomIndex);
-
-                if (searches.size() > 0) {
-                    randomIndex = random.nextInt(searches.size());
-
-                    testSession.add(searches.get(randomIndex));
-                    searches.remove(randomIndex);
-                }
-            }
-
-            trainingSessions.add(trainSession);
-            newTestingSessions.add(testSession);
-        }
-
-        testingSessions = newTestingSessions;
-    }
-
-    private Session copySession(final Session session) {
-        Session copy = new Session();
-
-        copy.setIntent(session.getIntent());
-        copy.setUserId(session.getUserId());
-
-        return copy;
     }
 
     public List<Session> getTrainingSessions() {
