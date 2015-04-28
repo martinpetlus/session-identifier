6991443513491e33da4a519b5594a4225615fe47
Martin Petluš <martinpetlus@gmail.com>
2015-04-29 01:45:33 +0200

    Rename static attr of TemporalDistanceConsecutiveApproach

--- src/sk/stuba/fiit/ms/session/identifiers/consecutive/TemporalDistanceConsecutiveApproach.java
+++ src/sk/stuba/fiit/ms/session/identifiers/consecutive/TemporalDistanceConsecutiveApproach.java
@@ -4,12 +4,12 @@ import sk.stuba.fiit.ms.session.Search;
 
 public final class TemporalDistanceConsecutiveApproach implements ConsecutiveApproach {
 
-    // 25.5 minutes
-    public static final long MAX_GAP_IN_MILLIS = 1530_000L;
+    // 25.5 minutes in milliseconds
+    public static final long MAX_GAP = 1530_000L;
 
     @Override
     public boolean isSameSession(final Search previous, final Search current) {
-        return (current.getTimeStamp() - previous.getTimeStamp()) <= MAX_GAP_IN_MILLIS;
+        return (current.getTimeStamp() - previous.getTimeStamp()) <= MAX_GAP;
     }
 
 }
