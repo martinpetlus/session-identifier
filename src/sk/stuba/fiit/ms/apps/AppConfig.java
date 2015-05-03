dfef66a836750cd374bbacc0783c5024cd7a38ae
Martin Petluš <martinpetlus@gmail.com>
2015-05-03 21:33:56 +0200

    Refactor configuration to allow load only session track sessions or Soke sessions at the same time

--- src/sk/stuba/fiit/ms/apps/AppConfig.java
+++ src/sk/stuba/fiit/ms/apps/AppConfig.java
@@ -16,55 +16,73 @@ public final class AppConfig {
     }
 
     public boolean useSessionTrack2011() {
-        return getBoolean("session_track_2011");
+        return getBoolean("2011", getSessionTrackSessions());
     }
 
     public boolean useSessionTrack2012() {
-        return getBoolean("session_track_2012");
+        return getBoolean("2012", getSessionTrackSessions());
     }
 
     public boolean useSessionTrack2013() {
-        return getBoolean("session_track_2013");
+        return getBoolean("2013", getSessionTrackSessions());
     }
 
     public boolean useSessionTrack2014() {
-        return getBoolean("session_track_2014");
+        return getBoolean("2014", getSessionTrackSessions());
     }
 
+    /**
+     * We can load sessions from session tracks (2011, 2012,
+     * 2013, 2014), or Soke sessions, but not both at same time.
+     * @return true if we are loading Soke sessions
+     */
     public boolean useSokeSessions() {
-        return getBoolean("soke_sessions");
+        if (useSessionTrack2011() || useSessionTrack2012() ||
+            useSessionTrack2013() || useSessionTrack2014()) {
+            return false;
+        } else {
+            return getBoolean("soke_sessions", jsonConfig);
+        }
+    }
+
+    private JSONObject getSessionTrackSessions() {
+        return getJSONObject("session_track_sessions", jsonConfig);
     }
 
     public boolean downloadContentsOfClickedResults() {
-        return getBoolean("download_contents_of_clicked_results");
+        return getBoolean("download_contents_of_clicked_results", jsonConfig);
     }
 
     public double getRatioBetweenTrainingAndTestingSessions() {
-        return getDouble("ratio_between_training_and_testing_sessions");
+        return getDouble("ratio_between_training_and_testing_sessions", jsonConfig);
     }
 
     public double getSVM_C() {
-        return getDouble("SVM_C");
+        return getDouble("SVM_C", jsonConfig);
     }
 
     public int getLDAIterations() {
-        return getInt("LDA_iterations");
+        return getInt("LDA_iterations", jsonConfig);
     }
 
     public int getLDATopics() {
-        return getInt("LDA_topics");
+        return getInt("LDA_topics", jsonConfig);
     }
 
-    private boolean getBoolean(final String property) {
-        return ((Boolean) jsonConfig.get(property)).booleanValue();
+    private static JSONObject getJSONObject(final String property, final JSONObject jsonObject) {
+        return (JSONObject) jsonObject.get(property);
     }
 
-    private double getDouble(final String property) {
-        return ((Double) jsonConfig.get(property)).doubleValue();
+    private static boolean getBoolean(final String property, final JSONObject jsonObject) {
+        return (Boolean) jsonObject.get(property);
     }
 
-    private int getInt(final String property) {
-        return ((Long) jsonConfig.get(property)).intValue();
+    private static double getDouble(final String property, final JSONObject jsonObject) {
+        return (Double) jsonObject.get(property);
+    }
+
+    private static int getInt(final String property, final JSONObject jsonObject) {
+        return ((Long) jsonObject.get(property)).intValue();
     }
 
     public static AppConfig load(final String configFile) throws IOException, ParseException {
